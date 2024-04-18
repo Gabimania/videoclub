e300da42567718ced5ed1c05a15c18c9553e6d8b
Gabimania <gabrielenlo9@gmail.com>
2024-04-18 20:29:21 +0200

    Employee insert film

--- employee.php
+++ employee.php
@@ -1,28 +1,107 @@
 <?php
-include("./templates/header.php");
-?>
-
-<?php
 session_start();
 if (isset($_SESSION["employee"])) {
     $name = $_SESSION["employee"];
-   
-} else {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        include("conection.php");
+
+        $idemployee = $_SESSION["idemployee"];
+
+       
+        $category = $_POST["category"];
+        $image = $_FILES["image"]["name"];
+        $target_dir = "assets/img/";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        // Verificar si la imagen es real o falsa
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["image"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+
+        // Verificar si el archivo ya existe
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        // Verificar el tamaño de la imagen
+        if ($_FILES["image"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+        // Permitir ciertos formatos de archivo
+        if (
+            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+        //Guardas imagen y actualizas bbddd
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+            // Si todo está bien, intenta subir el archivo
+        } else {
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                //echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+                // Preparar la consulta SQL para insertar los datos en la tabla de películas
+                $sql = "INSERT INTO film (name,img, idcategory, idemployee) VALUES (?, ?, ?, ?)";
+                $stmt = $conn->prepare($sql);
+    
+                // Vincular los parámetros
+
+                $stmt->bindParam(1, $name);
+                $stmt->bindParam(2, $image);
+                $stmt->bindParam(3, $category);
+                $stmt->bindParam(4, $idemployee);
+               
+    
+                // Ejecutar la consulta
+                try {
+                    $stmt->execute();
+                    echo "Movie added successfully.";
+                } catch (PDOException $e) {
+                    echo "Error: " . $e->getMessage();
+                }
+    
+                // Cerrar la conexión
+                $conn = null;
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    }
+
+    }
+ else {
     header("Location: ./");
 }
 
 ?>
-
+<?php
+include("./templates/header.php");
+?>
 <h1>Welcome <?php echo $name; ?></h1>
 <!-- Formulario para subir películas -->
 <form action="" method="post" enctype="multipart/form-data">
     <label for="movie_name">Movie Name:</label><br>
-    <input type="text" id="movie_name" name="movie_name"><br>
+    <input type="text" id="movie_name" name="name"><br>
     <label for="category">Category:</label><br>
     <select id="category" name="category">
         <?php
         include("conection.php");
-        $sql = "Select name from category";
+        $sql = "Select idcategory,name from category";
         $result = $conn->prepare($sql);
         $result->execute();
 
@@ -31,7 +110,7 @@ if (isset($_SESSION["employee"])) {
             $categorias = $result->fetchAll();
             // Iterar sobre las categorías y generar las opciones del select
             foreach ($categorias as $categoria) {
-                echo "<option value='" . $categoria['name'] . "'>" . $categoria['name'] . "</option>";
+                echo "<option value='" . $categoria['idcategory'] . "'>" . $categoria['name'] . "</option>";
             }
         } else {
             echo "No categories available";
@@ -39,7 +118,7 @@ if (isset($_SESSION["employee"])) {
         ?>
     </select><br><br>
     <label for="movie_image">Movie Image:</label><br>
-    <input type="file" id="movie_image" name="movie_image"><br><br>
+    <input type="file" id="movie_image" name="image" accept="image/*" required><br><br>
     <input type="submit" value="Upload Movie">
 </form>
 
